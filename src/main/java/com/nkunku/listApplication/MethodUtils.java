8ebfd8a723f32f1b08cfab3002080621fa92527e
MikeNkunku <mikenkunku@gmail.com>
2017-06-26 00:59:00 +0200

    Upgraded format of the console log for the getElapsedTime method

--- src/main/java/com/nkunku/listApplication/MethodUtils.java
+++ src/main/java/com/nkunku/listApplication/MethodUtils.java
@@ -2,6 +2,7 @@ package com.nkunku.listApplication;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * Class containing methods which are likely to be used by other classes.
@@ -11,15 +12,15 @@ public class MethodUtils {
 
 	/**
 	 * @param pMethod The method to invoke.
-	 * @param pArgs The arguments to call the method with.
+	 * @param pLists The arguments to call the method with.
 	 * @return The elapsed time.
 	 * @throws IllegalAccessException When the provided method cannot be accessed.
 	 * @throws IllegalArgumentException When the arguments are not of the correct type.
 	 * @throws InvocationTargetException When the method cannot be called on the object.
 	 */
-	public static long getElapsedTime(final Method pMethod, final String... pArgs) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static long getElapsedTime(final Method pMethod, final List<String>... pLists) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		long startTime = System.currentTimeMillis();
-		System.out.format("%s : %s\n", pMethod.getName(), pMethod.invoke(null, (Object[]) pArgs));
+		System.out.format("Method name: %s\nResult: %s\n", pMethod.getName(), pMethod.invoke(null, (Object[]) pLists));
 		long endTime = System.currentTimeMillis();
 		return endTime - startTime;
 	}
