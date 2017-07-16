9a510451b6fedc6139bd436a869d0270ea5f5d5c
MikeNkunku <mikenkunku@gmail.com>
2017-07-16 05:46:59 +0200

    [ADD & UPGRADE] UTs
    - Used static constants for lists ;
    - Added UTs for the intersection method.

--- src/test/java/com/nkunku/listApplication/backend/MyListUtilsTest.java
+++ src/test/java/com/nkunku/listApplication/backend/MyListUtilsTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.collection.IsCollectionWithSize.*;
 
 import java.util.List;
 
-
 import org.junit.Test;
 
 import com.nkunku.listApplication.ListApplicationException;
@@ -17,6 +16,41 @@ import com.nkunku.listApplication.backend.MyListUtils;
  * @author Mike.
  */
 public class MyListUtilsTest {
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+	// Constants
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	/** First list. */
+	private static List<String> LIST_1;
+
+	/** Second list. */
+	private static List<String> LIST_2;
+
+	/** First list with duplications. */
+	private static List<String> LIST_DUPLICATIONS_1;
+
+	/** Second list with duplications. */
+	private static List<String> LIST_DUPLICATIONS_2;
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+	// Setup methods
+	// --------------------------------------------------------------------------------------------------------------------------------
+
+	static {
+		try {
+			LIST_1 = MyListUtils.getListFromString("1,2,3", null);
+			LIST_2 = MyListUtils.getListFromString("4,5,6", null);
+			LIST_DUPLICATIONS_1 = MyListUtils.getListFromString("1,1,2,3,3", null);
+			LIST_DUPLICATIONS_2 = MyListUtils.getListFromString("2,2,4,5,5", null);
+		} catch (ListApplicationException e) {
+			System.out.format("%s unit tests could not be run due to static initialization errors\n", MyListUtilsTest.class.getSimpleName());
+		}
+	}
+
+	// --------------------------------------------------------------------------------------------------------------------------------
+	// Tests
+	// --------------------------------------------------------------------------------------------------------------------------------
 
 	@Test
 	public void testGetListFromStringNullListString() {
@@ -29,9 +63,8 @@ public class MyListUtilsTest {
 
 	@Test
 	public void testGetListFromStringValidListStringWithDfltDelimiter() throws ListApplicationException {
-		List<String> stringsList = MyListUtils.getListFromString("1,2", null);
-		assertThat("The list is supposed to have a size of 2", stringsList, hasSize(2));
-		assertThat("The list is supposed to contain the provided elements", stringsList, hasItems("1", "2"));
+		assertThat("The list is supposed to have a size of 3", LIST_1, hasSize(3));
+		assertThat("The list is supposed to contain the provided elements", LIST_1, hasItems("1", "2", "3"));
 	}
 
 	@Test
@@ -43,27 +76,42 @@ public class MyListUtilsTest {
 
 	@Test
 	public void testUnionSameList() throws ListApplicationException {
-		List<String> uniqueList = MyListUtils.getListFromString("1,2,3", null);
-		List<String> unionList = MyListUtils.union(uniqueList, uniqueList);
+		List<String> unionList = MyListUtils.union(LIST_1, LIST_1);
 		assertThat("The list is supposed to have 3 elements", unionList, hasSize(3));
 		assertThat("The union list is supposed to contain the provided elements", unionList, hasItems("1", "2", "3"));
 	}
 
 	@Test
 	public void testUnionDifferentLists() throws ListApplicationException {
-		List<String> l1 = MyListUtils.getListFromString("1,2,3", null);
-		List<String> l2 = MyListUtils.getListFromString("4,5,6", null);
-		List<String> unionList = MyListUtils.union(l1, l2);
+		List<String> unionList = MyListUtils.union(LIST_1, LIST_2);
 		assertThat("The list is supposed to have 6 elements", unionList, hasSize(6));
 		assertThat("The union of those 2 different lists is supposed to contain the provided elements", unionList, hasItems("1", "2", "3", "4", "5", "6"));
 	}
 
 	@Test
-	public void testUnionListsWithDoublons() throws ListApplicationException {
-		List<String> l1 = MyListUtils.getListFromString("1,1,2,3,3", null);
-		List<String> l2 = MyListUtils.getListFromString("2,2,4,5,5", null);
-		List<String> unionList = MyListUtils.union(l1, l2);
+	public void testUnionListsWithDuplications() throws ListApplicationException {
+		List<String> unionList = MyListUtils.union(LIST_DUPLICATIONS_1, LIST_DUPLICATIONS_2);
 		assertThat("The union of those lists with doublons should contain 9 elements", unionList, hasSize(9));
 		assertThat("The union of those lists with doublons should contain the provided elements", unionList, hasItems("1", "1", "2", "2", "3", "3", "4", "5", "5"));
+	}
+
+	@Test
+	public void testIntersectionWithSameList() throws ListApplicationException {
+		List<String> intersectList = MyListUtils.intersection(LIST_1, LIST_1);
+		assertThat("The intersection of the same list should contain 3 elements", intersectList, hasSize(3));
+		assertThat("The intersection of the same list should contain the provided elements", intersectList, hasItems("1", "2", "3"));
+	}
+
+	@Test
+	public void testInterserctionDifferentLists() throws ListApplicationException {
+		List<String> intersectList = MyListUtils.intersection(LIST_1, LIST_2);
+		assertTrue("The intersection of the two different lists should be empty", intersectList.isEmpty());
+	}
+
+	@Test
+	public void testIntersectionListsWithDuplications() throws ListApplicationException {
+		List<String> intersectList = MyListUtils.intersection(LIST_DUPLICATIONS_1, LIST_DUPLICATIONS_2);
+		assertThat("The intersection of the two lists with duplications should contain only one element", intersectList, hasSize(1));
+		assertThat("The intersection of the two lists with duplications should be \"2\"", intersectList, hasItem("2"));
 	}
 }
