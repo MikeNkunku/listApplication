9404f257c2da5af551adfc011bd93b29ea2024ad
MikeNkunku <mikenkunku@gmail.com>
2017-07-18 17:54:34 +0200

    [UPGRADE] MethodUtils.getMeanElapsedTime
    - getElapsedTime -> getMeanElapsedTime ;
    - Added the number of times which the invoked method must be run.

--- src/main/java/com/nkunku/listApplication/backend/MethodUtils.java
+++ src/main/java/com/nkunku/listApplication/backend/MethodUtils.java
@@ -12,16 +12,24 @@ public class MethodUtils {
 
 	/**
 	 * @param pMethod The method to invoke.
+	 * @param pNbRuns The number of times that the method must be executed.
 	 * @param pLists The arguments to call the method with.
-	 * @return The elapsed time.
+	 * @return The mean elapsed time (in <b>milliseconds</b>) for the method with the provided arguments.
 	 * @throws IllegalAccessException When the provided method cannot be accessed.
 	 * @throws IllegalArgumentException When the arguments are not of the correct type.
 	 * @throws InvocationTargetException When the method cannot be called on the object.
 	 */
-	public static long getElapsedTime(final Method pMethod, final List<String>... pLists) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		long startTime = System.currentTimeMillis();
-		System.out.format("Method name: %s\nResult: %s\n", pMethod.getName(), pMethod.invoke(null, (Object[]) pLists));
-		long endTime = System.currentTimeMillis();
-		return endTime - startTime;
+	public static long getMeanElapsedTime(final Method pMethod, final int pNbRuns, final List<String>... pLists) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		int nbRuns = pNbRuns < 2 ? 1 : pNbRuns;
+		int runIdx = 0;
+		long startTime;
+		long sumElapasedTimes = 0;
+		while (runIdx < nbRuns) {
+			startTime = System.currentTimeMillis();
+			pMethod.invoke(null, (Object[]) pLists);
+			sumElapasedTimes += System.currentTimeMillis() - startTime;
+			nbRuns++;
+		}
+		return sumElapasedTimes / nbRuns;
 	}
 }
