4ba52e1f5d07ee93dfb673e8a0a899b6436f7037
MikeNkunku <mikenkunku@gmail.com>
2017-07-19 08:51:45 +0200

    [ADD] Enumeration class
    Added enumeration class for the available list operations.

---
+++ src/main/java/com/nkunku/listApplication/backend/MyListUtilsOperation.java
@@ -0,0 +1,20 @@
+package com.nkunku.listApplication.backend;
+
+/**
+ * Enumeration class for the available operations of {@linkplain MyListUtils}.
+ * @author Mike.
+ */
+public enum MyListUtilsOperation {
+
+	/** Union operation. */
+	UNION,
+
+	/** Difference operation. */
+	DIFFERENCE,
+
+	/** Intersection operation. */
+	INTERSECTION,
+
+	/** Disjunction operation. */
+	DISJUNCTION;
+}
