1712e4762ec2ff506b1e234bb2a13273d8b4725e
MikeNkunku <mikenkunku@gmail.com>
2017-08-04 07:02:40 +0200

    Application run
    Now launching the main frame.

--- src/main/java/com/nkunku/listApplication/ListApplication.java
+++ src/main/java/com/nkunku/listApplication/ListApplication.java
@@ -1,11 +1,6 @@
 package com.nkunku.listApplication;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.List;
-
-import com.nkunku.listApplication.backend.MethodUtils;
-import com.nkunku.listApplication.backend.MyListUtils;
+import com.nkunku.listApplication.frontend.ListApplicationFrame;
 
 /**
  * Class to run the application.
@@ -13,14 +8,10 @@ import com.nkunku.listApplication.backend.MyListUtils;
  */
 public class ListApplication {
 
-	/** @param pArgs The application arguments. 
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 * @throws IllegalAccessException */
-	@SuppressWarnings("unchecked")
-	public static void main(final String[] pArgs) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		System.out.format("Time elapsed after call to MyListUtils.union(String, String) : %dms", MethodUtils.getElapsedTime(MyListUtils.class.getMethod("union", List.class, List.class), Arrays.asList("1"), Arrays.asList("2")));
+	/**
+	 * @param pArgs The application arguments.
+	 */
+	public static void main(final String[] pArgs) {
+		ListApplicationFrame.launch();
 	}
 }
