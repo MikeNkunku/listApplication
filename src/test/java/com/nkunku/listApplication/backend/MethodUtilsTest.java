7c4131e03477a2020c4a53e312d631a69f095ce9
MikeNkunku <mikenkunku@gmail.com>
2018-05-31 05:29:41 +0200

    Started migrated assertions from JUnit to AssertJ.
    - Also fixed build configuration (language level set to 1.8);
    - Added missing scope for Assert4j dependency.

--- src/test/java/com/nkunku/listApplication/backend/MethodUtilsTest.java
+++ src/test/java/com/nkunku/listApplication/backend/MethodUtilsTest.java
@@ -1,13 +1,12 @@
 package com.nkunku.listApplication.backend;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+
 
 /**
  * Unit testing class for {@link MethodUtils}.
@@ -15,36 +14,32 @@ import org.junit.Test;
  */
 public class MethodUtilsTest {
 
-	// --------------------------------------------------------------------------------------------------------------------------------
-	// Tests
-	// --------------------------------------------------------------------------------------------------------------------------------
-
 	@Test
-	public void testGetMeanElaspedTimeOnce() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	public void testGetMeanElaspedTimeOnce() throws Exception {
 		testGetMeanElapsedTime(-1);
 	}
 
 	@Test
-	public void testGetMeanElaspedTimeTenTimes() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	public void testGetMeanElaspedTimeTenTimes() throws Exception {
 		testGetMeanElapsedTime(10);
 	}
 
 	@Test
 	public void testGetMillisecondsFromNanoseconds() {
-		assertEquals("The result should be 1ms", 1, MethodUtils.getMillisecondsFromNanoseconds(900_000L));
-		assertEquals("The result should be 1ms", 1, MethodUtils.getMillisecondsFromNanoseconds(1_130_300L));
-		assertEquals("The result should be 1ms", 1, MethodUtils.getMillisecondsFromNanoseconds(1_499_999L));
-		assertEquals("The result should be 2ms", 2, MethodUtils.getMillisecondsFromNanoseconds(1_500_000L));
+		assertThat(MethodUtils.getMillisecondsFromNanoseconds(900_000L)).isEqualTo(1);
+		assertThat(MethodUtils.getMillisecondsFromNanoseconds(1_130_300L)).isEqualTo(1);
+		assertThat(MethodUtils.getMillisecondsFromNanoseconds(1_499_999L)).isEqualTo(1);
+		assertThat(MethodUtils.getMillisecondsFromNanoseconds(1_500_000L)).isEqualTo(2);
 	}
 
-	// --------------------------------------------------------------------------------------------------------------------------------
-	// Factorized methods to test
-	// --------------------------------------------------------------------------------------------------------------------------------
 
-	@SuppressWarnings("unchecked")
-	private void testGetMeanElapsedTime(final int pNbRuns) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	/**
+	 * @param   pNbRuns     The number of times to run the {@linkplain MyListUtils#union union} method.
+	 * @throws  Exception   When the union method didn't get the right parameters or an error occurred while computing the mean elapsed time.
+	 */
+	private void testGetMeanElapsedTime(final int pNbRuns) throws Exception {
 		long meanElapsedTime = MethodUtils.getMeanElapsedTime(MyListUtils.class.getMethod("union", List.class, List.class), pNbRuns, Arrays.asList("1", "2"),
 				Arrays.asList("1", "2"));
-		assertTrue("The mean of the elapsed time should be valid", meanElapsedTime > 0);
+		assertThat(meanElapsedTime).isGreaterThan(0);
 	}
 }
--- src/test/java/com/nkunku/listApplication/backend/MyListUtilsTest.java
+++ src/test/java/com/nkunku/listApplication/backend/MyListUtilsTest.java
@@ -1,26 +1,18 @@
 package com.nkunku.listApplication.backend;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.collection.IsCollectionWithSize.*;
-
-import java.util.Collections;
-import java.util.List;
-
+import com.nkunku.listApplication.ListApplicationException;
+import org.assertj.core.api.Condition;
 import org.junit.Test;
 
-import com.nkunku.listApplication.ListApplicationException;
-import com.nkunku.listApplication.backend.MyListUtils;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Unit testing class for {@link MyListUtils}.
  * @author Mike.
  */
 public class MyListUtilsTest {
-
-	// --------------------------------------------------------------------------------------------------------------------------------
-	// Constants
-	// --------------------------------------------------------------------------------------------------------------------------------
 
 	/** First list. */
 	private static List<String> LIST_1;
@@ -40,9 +32,6 @@ public class MyListUtilsTest {
 	/** Second list with duplications. */
 	private static List<String> LIST_DUPLICATIONS_2;
 
-	// --------------------------------------------------------------------------------------------------------------------------------
-	// Setup methods
-	// --------------------------------------------------------------------------------------------------------------------------------
 
 	static {
 		try {
@@ -57,145 +46,147 @@ public class MyListUtilsTest {
 		}
 	}
 
-	// --------------------------------------------------------------------------------------------------------------------------------
-	// Tests
-	// --------------------------------------------------------------------------------------------------------------------------------
 
 	@Test
 	public void testGetListFromStringNullListString() {
 		try {
 			MyListUtils.getListFromString(null, null);
 		} catch (ListApplicationException e) {
-			assertThat("The list string was expected to be empty", e.getMessage(), equalTo("The list is invalid"));
+			assertThat(e.getMessage()).isEqualTo("The list \"null\" is invalid.");
 		}
 	}
 
 	@Test
-	public void testGetListFromStringValidListStringWithDfltDelimiter() throws ListApplicationException {
-		assertThat("The list is supposed to have a size of 3", LIST_1, hasSize(3));
-		assertThat("The list is supposed to contain the provided elements", LIST_1, hasItems("1", "2", "3"));
+	public void testGetListFromStringValidListStringWithDfltDelimiter() {
+		assertThat(LIST_1)
+			.hasSize(3)
+			.contains("1", "2", "3");
 	}
 
 	@Test
 	public void testGetListFromStringValidListWithUserDelimiter() throws ListApplicationException {
-		List<String> list = MyListUtils.getListFromString("1;3;4;4;6", ";");
-		assertThat("The list is supposed to have 5 elements", list, hasSize(5));
-		assertThat("The list should contain the 5 provided elements", list, hasItems("1", "3", "6"));
-		assertEquals("The list should contain \"4\" twice", 2, Collections.frequency(list, "4"));
+		assertThat(MyListUtils.getListFromString("1;3;4;4;6", ";"))
+			.hasSize(5)
+			.containsOnlyOnce("1", "3", "6")
+			.haveExactly(2, new Condition<>("4"::equals, "4 is present twice"));
 	}
 
 	@Test
-	public void testUnionSameList() throws ListApplicationException {
-		List<String> unionList = MyListUtils.union(LIST_1, LIST_1);
-		assertThat("The list is supposed to have 3 elements", unionList, hasSize(3));
-		assertThat("The union list is supposed to contain the provided elements", unionList, hasItems("1", "2", "3"));
+	public void testUnionSameList() {
+		assertThat(MyListUtils.union(LIST_1, LIST_1))
+			.hasSize(3)
+			.contains("1", "2", "3");
 	}
 
 	@Test
-	public void testUnionDifferentLists() throws ListApplicationException {
-		List<String> unionList = MyListUtils.union(LIST_1, LIST_2);
-		assertThat("The list is supposed to have 6 elements", unionList, hasSize(6));
-		assertThat("The union of those 2 different lists is supposed to contain the provided elements", unionList,
-				hasItems("1", "2", "3", "4", "5", "6"));
+	public void testUnionDifferentLists() {
+		assertThat(MyListUtils.union(LIST_1, LIST_2))
+			.hasSize(6)
+			.containsOnlyOnce("1", "2", "3", "4", "5", "6");
 	}
 
 	@Test
-	public void testUnionListsWithDuplications() throws ListApplicationException {
+	public void testUnionListsWithDuplications() {
 		List<String> unionList = MyListUtils.union(LIST_DUPLICATIONS_1, LIST_DUPLICATIONS_2);
-		String msgPrefix = "The union of those lists with doublons should contain";
-		assertThat(String.format("%s 9 elements", msgPrefix), unionList, hasSize(9));
+		assertThat(unionList)
+			.hasSize(9)
+			.containsOnlyOnce("4");
+
 		String[] eltsInDouble = new String[] {"1", "2", "3", "5"};
 		for (String elt : eltsInDouble) {
-			assertEquals(String.format("%s \"%s\" twice", msgPrefix, elt), 2, Collections.frequency(unionList, elt));
+			assertPresentTwice(unionList, elt);
 		}
-		assertThat(String.format("%s contain the provided elements", msgPrefix), unionList, hasItem("4"));
 	}
 
 	@Test
-	public void testIntersectionWithSameList() throws ListApplicationException {
-		List<String> intersectList = MyListUtils.intersection(LIST_1, LIST_1);
-		String msgPrefix = "The intersection of the same list should contain";
-		assertThat(String.format("%s 3 elements", msgPrefix), intersectList, hasSize(3));
-		assertThat(String.format("%s the provided elements", msgPrefix), intersectList, hasItems("1", "2", "3"));
+	public void testIntersectionWithSameList() {
+		assertThat(MyListUtils.intersection(LIST_1, LIST_1))
+			.hasSize(3)
+			.contains("1", "2", "3");
 	}
 
 	@Test
-	public void testInterserctionDifferentLists() throws ListApplicationException {
-		List<String> intersectList = MyListUtils.intersection(LIST_1, LIST_2);
-		assertTrue("The intersection of the two different lists should be empty", intersectList.isEmpty());
+	public void testIntersectionDifferentLists() {
+		assertThat(MyListUtils.intersection(LIST_1, LIST_2)).isEmpty();
 	}
 
 	@Test
-	public void testIntersectionListsWithDuplications() throws ListApplicationException {
-		List<String> intersectList = MyListUtils.intersection(LIST_DUPLICATIONS_1, LIST_DUPLICATIONS_2);
-		String msgPrefix = "The intersection of the two lists with duplications should";
-		assertThat(String.format("%s contain only one element", msgPrefix), intersectList, hasSize(1));
-		assertThat(String.format("%s be \"2\"", msgPrefix), intersectList, hasItem("2"));
+	public void testIntersectionListsWithDuplications() {
+		assertThat(MyListUtils.intersection(LIST_DUPLICATIONS_1, LIST_DUPLICATIONS_2))
+			.hasSize(1)
+			.containsOnly("2");
 	}
 
 	@Test
-	public void testIntersectionListsWithCommonDuplications() throws ListApplicationException {
+	public void testIntersectionListsWithCommonDuplications() {
 		List<String> intersectionList = MyListUtils.intersection(LIST_COMMON_DUPLICATIONS_1, LIST_COMMON_DUPLICATIONS_2);
-		String msgPrefix = "The intersection of the two lists with common duplications should contain";
-		assertThat(String.format("%s contain 6 elements", msgPrefix), intersectionList, hasSize(6));
+		assertThat(intersectionList)
+			.hasSize(6)
+			.containsOnlyOnce("2", "3");
+
 		String[] eltsInDouble = new String[] {"1", "5"};
 		for (String elt : eltsInDouble) {
-			assertEquals(String.format("%s \"%s\" twice", msgPrefix, elt), 2, Collections.frequency(intersectionList, elt));
+			assertPresentTwice(intersectionList, elt);
 		}
-		assertThat(String.format("%s the provided elements", msgPrefix), intersectionList, hasItems("2", "3"));
 	}
 
 	@Test
-	public void testDifferenceSameList() throws ListApplicationException {
-		List<String> differenceList = MyListUtils.difference(LIST_1, LIST_1);
-		assertTrue("The difference of the same list should be empty", differenceList.isEmpty());
+	public void testDifferenceSameList() {
+		assertThat(MyListUtils.difference(LIST_1, LIST_1)).isEmpty();
 	}
 
 	@Test
-	public void testDifferenceDifferentLists() throws ListApplicationException {
-		List<String> differenceList = MyListUtils.difference(LIST_1, LIST_2);
-		String msgPfx = "The difference of those 2 lists should";
-		assertThat(String.format("%s contain 3 elements", msgPfx), differenceList, hasSize(3));
-		assertThat(String.format("%s be equal to the \"LIST_1\"", msgPfx), differenceList, equalTo(LIST_1));
-	}
- 
-	@Test
-	public void testDifferenceListsWithCommonDuplications() throws ListApplicationException {
-		List<String> differenceList = MyListUtils.difference(LIST_COMMON_DUPLICATIONS_1, LIST_COMMON_DUPLICATIONS_2);
-		String msgPfx = "The difference of the two lists with common duplication should";
-		assertThat(String.format("%s contain %d elements", msgPfx, 2), differenceList, hasSize(2));
-		assertThat(String.format("%s contain the provided elements", msgPfx), differenceList, hasItems("3", "5"));
+	public void testDifferenceDifferentLists() {
+		assertThat(MyListUtils.difference(LIST_1, LIST_2))
+			.hasSize(3)
+			.isEqualTo(LIST_1);
 	}
 
 	@Test
-	public void testDisjunctionWithSameList() throws ListApplicationException {
-		List<String> disjunctionList = MyListUtils.disjunction(LIST_1, LIST_1);
-		assertTrue("The disjunction of the same list should be empty", disjunctionList.isEmpty());
+	public void testDifferenceListsWithCommonDuplications() {
+		assertThat(MyListUtils.difference(LIST_COMMON_DUPLICATIONS_1, LIST_COMMON_DUPLICATIONS_2))
+			.hasSize(2)
+			.containsOnlyOnce("3", "5");
 	}
 
 	@Test
-	public void testDisjunctionWithDifferentLists() throws ListApplicationException {
-		List<String> disjunctionList = MyListUtils.disjunction(LIST_1, LIST_2);
-		String msgPfx = "The disjunction of those 2 different lists should";
-		assertThat(String.format("%s contain %d elements", msgPfx, 6), disjunctionList, hasSize(6));
-		assertThat(String.format("%s contain the provided elements", msgPfx), disjunctionList, hasItems("1", "2", "3", "4", "5", "6"));
+	public void testDisjunctionWithSameList() {
+		assertThat(MyListUtils.disjunction(LIST_1, LIST_1)).isEmpty();
 	}
 
 	@Test
-	public void testDisjunctionListsWithDuplications() throws ListApplicationException {
+	public void testDisjunctionWithDifferentLists() {
+		assertThat(MyListUtils.disjunction(LIST_1, LIST_2))
+			.hasSize(6)
+			.containsOnlyOnce("1", "2", "3", "4", "5", "6");
+	}
+
+	@Test
+	public void testDisjunctionListsWithDuplications() {
 		List<String> disjunctionList = MyListUtils.disjunction(LIST_DUPLICATIONS_1, LIST_DUPLICATIONS_2);
-		assertThat("The disjunction of those two lists with duplications should contain 8 elements", disjunctionList, hasSize(8));
-		assertEquals(String.format("The \"%s\" should occur %d times", "1", 2), 2, Collections.frequency(disjunctionList, "1"));
-		assertEquals(String.format("The \"%s\" should occur %d times", "2", 1), 1, Collections.frequency(disjunctionList, "2"));
-		assertEquals(String.format("The \"%s\" should occur %d times", "3", 2), 2, Collections.frequency(disjunctionList, "3"));
-		assertEquals(String.format("The \"%s\" should occur %d times", "4", 1), 1, Collections.frequency(disjunctionList, "4"));
-		assertEquals(String.format("The \"%s\" should occur %d times", "5", 2), 2, Collections.frequency(disjunctionList, "5"));
+		assertThat(disjunctionList)
+			.hasSize(8)
+			.containsOnlyOnce("2", "4");
+
+		String[] eltsInDouble = new String[] {"1", "3", "5"};
+		for (String elt : eltsInDouble) {
+			assertPresentTwice(disjunctionList, elt);
+		}
 	}
 
 	@Test
-	public void testDisjunctionListsWithCommonDuplications() throws ListApplicationException {
-		List<String> disjunctionList = MyListUtils.disjunction(LIST_COMMON_DUPLICATIONS_1, LIST_COMMON_DUPLICATIONS_2);
-		assertThat("The disjunction should contain 3 elements", disjunctionList, hasSize(3));
-		assertThat("The disjunction should contain the provided elements", disjunctionList, hasItems("2", "3", "5"));
+	public void testDisjunctionListsWithCommonDuplications() {
+		assertThat(MyListUtils.disjunction(LIST_COMMON_DUPLICATIONS_1, LIST_COMMON_DUPLICATIONS_2))
+			.hasSize(3)
+			.containsOnlyOnce("2", "3", "5");
+	}
+
+
+	/**
+	 * @param   pList   The list to assert on.
+	 * @param   pElt    The element which is to be present twice.
+	 */
+	private void assertPresentTwice (final List<String> pList, final String pElt) {
+		assertThat(pList).haveExactly(2, new Condition<>(pElt::equals, "contains twice"));
 	}
 }
