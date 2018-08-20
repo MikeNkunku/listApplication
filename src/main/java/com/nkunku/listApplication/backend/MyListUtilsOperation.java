dac4f1d4af1babf74e8a51b3e1eb54464fe75065
MikeNkunku <mikenkunku@gmail.com>
2018-08-20 12:27:31 +0200

    Added "equivalent" field in MLUO + Added associated UTs.

--- src/main/java/com/nkunku/listApplication/backend/MyListUtilsOperation.java
+++ src/main/java/com/nkunku/listApplication/backend/MyListUtilsOperation.java
@@ -10,32 +10,42 @@ import java.util.function.BiFunction;
 public enum MyListUtilsOperation {
 
 	/** Union operation. */
-	UNION(MyListUtils::union),
+	UNION(MyListUtils::union, "union"),
 	/** Difference operation. */
-	DIFFERENCE(MyListUtils::difference),
+	DIFFERENCE(MyListUtils::difference, "substract"),
 	/** Intersection operation. */
-	INTERSECTION(MyListUtils::intersection),
+	INTERSECTION(MyListUtils::intersection, "intersection"),
 	/** Disjunction operation. */
-	DISJUNCTION(MyListUtils::disjunction);
+	DISJUNCTION(MyListUtils::disjunction, "disjunction");
 
 	/** List operation to execute. */
 	private final BiFunction<List<String>, List<String>, List<String>> method;
+	/** The name of the equivalent method within {@linkplain org.apache.commons.collections.CollectionUtils CollectionUtils}. */
+	private final String equivalent;
 
 
 	/**
 	 * Private constructor.
-	 * @param pMethod The list operation to execute.
+	 * @param pMethod		The list operation to execute.
+	 * @param pEquivalent	The name of the equivalent method in CollectionUtils.
 	 */
-	MyListUtilsOperation(final BiFunction<List<String>, List<String>, List<String>> pMethod) {
+	MyListUtilsOperation(final BiFunction<List<String>, List<String>, List<String>> pMethod, final String pEquivalent) {
 		method = pMethod;
+		equivalent = pEquivalent;
 	}
+
 
 	/**
 	 * @param pList1 The first list provided.
-	 * @param pList2 The first list provided.
+	 * @param pList2 The second list provided.
 	 * @return The resulting list.
 	 */
 	public List<String> execute(final List<String> pList1, final List<String> pList2) {
 		return method.apply(pList1, pList2);
+	}
+
+	/** @return The name of the equivalent method within {@linkplain org.apache.commons.collections.CollectionUtils CollectionUtils}. */
+	public String equivalent() {
+		return equivalent;
 	}
 }
--- src/test/java/com/nkunku/listApplication/backend/MyListUtilsOperationTest.java
+++ src/test/java/com/nkunku/listApplication/backend/MyListUtilsOperationTest.java
@@ -3,13 +3,19 @@ package com.nkunku.listApplication.backend;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * UT class for {@link MyListUtilsOperation}.
  */
+@RunWith(Enclosed.class)
 public class MyListUtilsOperationTest {
 
 	/** One of the lists to provide in tests. */
@@ -18,27 +24,63 @@ public class MyListUtilsOperationTest {
 	private static final List<String> LIST_2 = Arrays.asList("3", "4", "5");
 
 
-	@Test
-	public void union() {
-		List<String> result = MyListUtilsOperation.UNION.execute(LIST_1, LIST_2);
-		assertThat(result).hasSize(5).contains("1", "2", "3", "4", "5");
+	/** Inner class dedicated to the testing of the equivalent parameter. */
+	@RunWith(Parameterized.class)
+	public static class EquivalentTest {
+
+		/** {@link MyListUtilsOperation} instance. */
+		private MyListUtilsOperation fInstance;
+		/** The expected equivalent for the instance. */
+		private String fExpectedEquivalent;
+
+		public EquivalentTest(final MyListUtilsOperation pInstance, final String pExpectedEquivalent) {
+			fInstance = pInstance;
+			fExpectedEquivalent = pExpectedEquivalent;
+		}
+
+		@Parameterized.Parameters(name = "{0}")
+		public static Collection<Object[]> data() {
+			return Arrays.asList(
+				new Object[]{ MyListUtilsOperation.UNION, "union" },
+				new Object[]{ MyListUtilsOperation.DIFFERENCE, "substract" },
+				new Object[]{ MyListUtilsOperation.DISJUNCTION, "disjunction" },
+				new Object[]{ MyListUtilsOperation.INTERSECTION, "intersection" }
+			);
+		}
+
+		@Test
+		public void equivalent() {
+			assertThat(fInstance.equivalent()).isEqualTo(fExpectedEquivalent);
+		}
 	}
 
-	@Test
-	public void difference() {
-		List<String> result = MyListUtilsOperation.DIFFERENCE.execute(LIST_1, LIST_2);
-		assertThat(result).hasSize(2).contains("1", "2");
-	}
 
-	@Test
-	public void intersection() {
-		List<String> result = MyListUtilsOperation.INTERSECTION.execute(LIST_1, LIST_2);
-		assertThat(result).containsOnly("3");
-	}
+	/**
+	 * Inner test class dedicated to the testing of the method.
+	 */
+	public static class MethodTest {
+		@Test
+		public void union() {
+			List<String> result = MyListUtilsOperation.UNION.execute(LIST_1, LIST_2);
+			assertThat(result).hasSize(5).contains("1", "2", "3", "4", "5");
+		}
 
-	@Test
-	public void disjunction() {
-		List<String> result = MyListUtilsOperation.DISJUNCTION.execute(LIST_1, LIST_2);
-		assertThat(result).hasSize(4).contains("1", "2", "4", "5");
+		@Test
+		public void difference() {
+			List<String> result = MyListUtilsOperation.DIFFERENCE.execute(LIST_1, LIST_2);
+			assertThat(result).hasSize(2).contains("1", "2");
+		}
+
+		@Test
+		public void intersection() {
+			List<String> result = MyListUtilsOperation.INTERSECTION.execute(LIST_1, LIST_2);
+			assertThat(result).containsOnly("3");
+		}
+
+		@Test
+		public void disjunction() {
+			List<String> result = MyListUtilsOperation.DISJUNCTION.execute(LIST_1, LIST_2);
+			assertThat(result).hasSize(4).contains("1", "2", "4", "5");
+		}
 	}
 }
